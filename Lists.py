42682eabd36b8d666933267d1932d8f76faefecd
rutrut6969 <rutledgeisaac6969@gmail.com>
2019-06-29 17:45:49 -0400

    Lists

---
+++ Lists.py
@@ -0,0 +1,9 @@
+#A list is a value that contains other values
+spam = ['cat', 'bat', 'rat']
+print(spam[0]) # Will print cat, because it is the first value in the list
+print(spam[1]) # Will print bat
+print(spam[2]) # And finally this will print rat
+
+spam2 = [['cat', 'bat'], [10, 20, 30, 40, 50]]
+print(spam2[0][1]) #prints cat
+print(spam2[1][4]) #prints 40
